ee8622a5a53587f718d805dba0d16433d7c012db
Ritesh Shukla2 <riteshshukla2@M-LD4JMWLW26.local>
2024-12-03 20:34:33 +0530

    [MOD] Add support for Keys

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,11 @@ const TurboEncryption = require('./NativeTurboEncryption').default;
 
 export function encrypt(plainText: string, key: string): string {
   console.log(key.length)
-  // if (key.length !== 16) {
-  //   throw new Error("Key must be 16 characters long");
-  // }
   return TurboEncryption.encrypt(plainText, key);
 }
 
 
 export function decrypt(encryptedText: string, key: string): string {
   console.log(key.length)
-  // if (key.length !== 16) {
-  //   throw new Error("Key must be 16 characters long");
-  // }
   return TurboEncryption.decrypt(encryptedText, key);
 }
