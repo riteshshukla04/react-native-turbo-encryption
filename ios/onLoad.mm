db46c3e980d2e790e0a90a49aa730d4716ca170b
Ritesh Shukla2 <riteshshukla2@M-LD4JMWLW26.local>
2024-12-05 00:14:27 +0530

    Feat:- Adding Cxx for IOS too

---
+++ ios/onLoad.mm
@@ -0,0 +1,29 @@
+
+//
+//  TurboEncryption.mm
+//  react-native-turbo-encryption
+//
+//  Created by Ritesh Shukla on 5.12.24.
+//
+
+
+#import "NativeTurboEncryptionTurboModule.h"
+#import <Foundation/Foundation.h>
+#import <ReactCommon/CxxTurboModuleUtils.h>
+
+@interface OnLoad : NSObject
+@end
+
+@implementation OnLoad
+
++ (void)load
+{
+    facebook::react::registerCxxModuleToGlobalModuleMap(
+        std::string(facebook::react::NativeTurboEncryptionTurboModule::kModuleName),
+        [](std::shared_ptr<facebook::react::CallInvoker> jsInvoker)
+        {
+            return std::make_shared<facebook::react::NativeTurboEncryptionTurboModule>(jsInvoker);
+        });
+}
+
+@end
