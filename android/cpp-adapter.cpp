0ea14bdf04beb5d221d6b2bd7ecebfb6dd4ac0e4
Ritesh Shukla <riteshshukla2381@gmail.com>
2024-12-16 01:00:05 +0530

    Remove Unwanted Code

--- android/cpp-adapter.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include "react-native-turbo-encryption.h"
-
-extern "C"
-JNIEXPORT jdouble JNICALL
-Java_com_turboencryption_TurboEncryptionModule_nativeMultiply(JNIEnv *env, jclass type, jdouble a, jdouble b) {
-    return turboencryption::multiply(a, b);
-}
